68cd87f84f969bd4b9500465b879d5ac73023f3c
Andres Cano <andres.canos@perficient.com>
2022-09-23 11:13:09 -0500

    Great session talking about react context and setting up our app logic within the app context�

--- coffe-experience/src/components/CoffeeCards/CoffeeCards.js
+++ coffe-experience/src/components/CoffeeCards/CoffeeCards.js
@@ -3,17 +3,19 @@ import Expandable from "../Expandable/Expandable";
 import Search from "../Search/Search";
 import './CoffeeCards.css';
 import {SwitchTransition, CSSTransition} from 'react-transition-group';
-import CoffeeContext from "../../store/coffee-context";
+import CoffeeContext, { useStore } from "../../store/coffee-context";
 import React, {useContext} from "react";
 // import ExpandableParagraph from "../Expandable/ExpandableParagraph";
 
+// destructuring
 const CoffeeCards = () => {
-  const coffeeCxt = useContext(CoffeeContext);
+  const { state: { coffeeSelectedData: { coffeeState } } } = useStore();
+
   return (
     <div className='cardContainer'>
       <SwitchTransition mode="out-in">
         <CSSTransition
-          key={coffeeCxt.coffeeState.coffeeSelected}
+          key={coffeeSelected}
           timeout={800}
           classNames='card_animation'
         >
--- coffe-experience/src/store/coffee-context.js
+++ coffe-experience/src/store/coffee-context.js
@@ -1,8 +1,9 @@
-import React, {useEffect, useState, useReducer} from "react";
+import React, {useEffect, useState, useReducer, useContext, useMemo} from "react";
 import PropTypes from 'prop-types';
 import { authRequest, requestHandler } from "../fetchers/fetchers";
 
-const initialState = {
+// lines from 5 trough 28 are a file
+const coffeeSelectedDataInitialState = {
   id: '0',
   title: 'Busca tu mejor sabor.',
   ingredients: [],
@@ -11,42 +12,89 @@ const initialState = {
   image: '../assests/coffeeImages/default.png'
 };
 
-const CoffeeContext = React.createContext({
-  coffeeState: {
-    coffeeSelected: 'default',
-    allCoffeeData: [],
-    coffeeSelectedData: initialState,
-    coffeeSelectionHandler: () => {}
-  },
-  authState: {
-    isLoggedIn: false,
-    logInHandler: () => {},
-    logInSetter: () => {}
-  }
-});
-
-const coffeeReducer = (state, action) => {
-  switch (action.type) {
-    case 'ALL_COFFEE_DATA':
-      return {allCoffeeData: action.value}
-    default:
-      return;
-  }
+const selectedCoffee = {
+  coffeeData: undefined,
+  coffeeSelectedData: coffeeSelectedDataInitialState,
 }
 
-const reducerCoffeeInitialState = {
-  coffeeSelected: '',
-  coffeeData: undefined,
-  coffeeSelectedData: initialState,
+const initialState = Object.freeze({
+  state: {
+    coffeeState: selectedCoffee,
+    authState: {
+      isLoggedIn: false,
+      logInHandler: () => {},
+      logInSetter: () => {}
+    },
+  },
+  dispatch: () => { }
+});
+
+// lines from 30 trough 56 are a file
+const ACTIONS = Object.freeze({
+  SET_SELECTED_COFFEE: 'SET_SELECTED_COFFEE',
+  SET_COFFE_INITIAL_DATA: 'SET_COFFE_INITIAL_DATA',
+  SET_PASSWORD: 'SET_PASSWORD',
+  SET_EMAIL: 'SET_EMAIL',
+});
+
+const setSelectedCoffee = (payload) => ({
+  type: ACTIONS.SET_SELECTED_COFFEE,
+  payload
+});
+
+const setCoffeeInitialData = (payload) => ({
+  type: ACTIONS.SET_COFFE_INITIAL_DATA,
+  payload
+});
+
+const setPassword= (payload) => ({
+  type: ACTIONS.SET_PASSWORD,
+  payload
+});
+
+const setEmail= (payload) => ({
+  type: ACTIONS.SET_EMAIL,
+  payload
+});
+
+// lines from 57 trough 61 are a file
+const AppContext = React.createContext({
+  state: undefined,
+  dispatch: undefined
+});
+
+export const useStore = () => useContext(AppContext);
+
+const appReducer = (state, action) => {
+  switch (action.type) {
+    case ACTIONS.SET_SELECTED_COFFEE:
+      return {
+        ...state,
+        coffeeSelectedData: state.coffeeData.find(coffee => coffee.title === action.payload)
+      }
+    case ACTIONS.SET_COFFE_INITIAL_DATA:
+      return {
+        ...state,
+        coffeeData: payload
+      }
+    case ACTIONS.SET_PASSWORD:
+      return {
+        ...state,
+        // todo
+      }
+    case ACTIONS.SET_EMAIL:
+      return {
+        ...state,
+        // todo
+      }
+    default:
+      throw new Error('Action not supported')
+  }
 }
 
 export const CoffeeContextProvider = (props) => {
-  const [coffeeStateReducer, dispatchCoffee] = useReducer(coffeeReducer, reducerCoffeeInitialState)
+  const [appState, dispatch] = useReducer(appReducer, initialState)
   
-  const [coffeeSelected, setCoffeeSelected] = useState('');
-  const [coffeeData, setCoffeeData] = useState(undefined);
-  const [coffeeSelectedData, setCoffeeSelectedData] = useState(initialState);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
   // const [token, setToken] = useState()
 
   // const requestHandler = useCallback(async () => {
@@ -103,18 +151,15 @@ export const CoffeeContextProvider = (props) => {
     requestHandler();
   }, []);
 
-  const coffeeSelectedHandler = (value) => {
-    // requestHandler();
-    setCoffeeSelected(value);
-    const dataSet = coffeeData.find((element) => element.title === value )
-    console.log(dataSet);
-    if (dataSet) {
-      setCoffeeSelectedData(dataSet);
-    } else {
-      setCoffeeSelectedData(initialState)
-    }
+  /**
+   * Change selected coffee
+   * @param {string} input 
+   */
+  const coffeeSelectedHandler = (input) => {
+    dispatch(setSelectedCoffee(input));
   };
 
+  // todo
   const logInHandler =  (email, password) => {
     authRequest(email, password);
   };
@@ -123,20 +168,19 @@ export const CoffeeContextProvider = (props) => {
     setIsLoggedIn(value);
   };
 
+  const value = useMemo(() => ({
+    state: {
+      ...appState,
+    },
+    dispatch,
+    coffeeSelectionHandler,
+    logInHandler,
+    logInSetter,
+    coffeeSelectedHandler
+  }), [appState]);
+
   return (
-    <CoffeeContext.Provider value={{
-      coffeeState: {
-        coffeeSelected: coffeeSelected,
-        allCoffeeData: coffeeData,
-        coffeeSelectedData: coffeeSelectedData,
-        coffeeSelectionHandler: coffeeSelectedHandler
-      },
-      authContext: {
-        isLoggedIn: isLoggedIn,
-        logInHandler: logInHandler,
-        logInSetter: logInSetter
-      }
-    }}>
+    <CoffeeContext.Provider value={value}>
       {props.children}
     </CoffeeContext.Provider>
   )
